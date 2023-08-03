f3756ddbba9240932214a4c1268d89420a79d1fb
k.molokov <k.molokov@atol.ru>
2023-08-04 01:16:16 +0300

    hotfix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ AppiumExtended is a collection of utilities and convenience functions designed t
 
 setup(
     name='AppiumExtended',
-    version='0.1.15',
+    version='0.1.16',
     description='An extension library for adding ease of use Appium-Python-Client',
     author='molokov-klim',
     packages=find_packages(),
